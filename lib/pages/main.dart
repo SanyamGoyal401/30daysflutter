6a922aaa53349e6c740967cb72237c3f25e97098
Sanyam <sanyamgoyal2859@gmail.com>
2022-11-02 00:20:17 +0530

    login page

---
+++ lib/pages/main.dart
@@ -0,0 +1,26 @@
+import 'package:days/pages/home_page.dart';
+import 'package:days/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.amber),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+}
